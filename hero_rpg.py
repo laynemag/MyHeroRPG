803b69564905e300b494e405b040368635ab5d19
Layne Magnuson <laynemag@gmail.com>
2020-11-19 15:50:42 -0600

    modified taking damage

--- hero_rpg.py
+++ hero_rpg.py
@@ -7,10 +7,9 @@ class Character:
         self.power = power
     
     def attack(self, enemy):            
-        enemy.health -= self.power
-        print(f"You do {self.power} damage to the {enemy.name}.")
-        if enemy.health <= 0:
-                print(f"The {enemy.name} is dead.")
+        #enemy.health -= self.power <-- implementing a take_damage feature
+        print(f"The {self.name} attacks {enemy.name}!")
+        enemy.take_damage(self.power)
     
     def is_alive(self):
             if self.health > 0:
@@ -21,21 +20,46 @@ class Character:
             print(f"Your health is {self.health} and your power is {self.power}")
         else:
             print(f"The {self.name}'s health is {self.health} and its power is {self.power}")
+    
+    def take_damage(self, damage):
+        self.health -= damage
+        print(f"{self.name} takes {damage} damage")
+        if self.health <= 0:
+            print(f"The {self.name} is dead!")
 
 class Hero(Character):
     def __init__(self, name, health, power,):
         self.name = name
         self.health = health
         self.power = power
-    
+    def attack(self, enemy):
+        print(f"You attack the {enemy.name}!")
+        attack_chance = random.randint(1,10)
+        if attack_chance >= 8:
+            self.power *= 2
+            print(f"Landed a critical strike on the {enemy.name}!") 
+        enemy.take_damage(self.power)
+        self.power = 60
 
-    
 class Goblin(Character):
-    def __init__(self, name, health, power,):
+    def __init__(self, name, health, power):
         self.name = name
         self.health = health
         self.power = power
 
+class Medic(Character):
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+
+    def attack(self, enemy):
+        enemy.health -= self.power
+        heal_chance = random.randint(1,10)
+        if self.health < 100 and heal_chance >= 8:
+            self.health += 20
+            print(f"The {self.name} just healed 20 health!")
+        
 
 cool_guy = Hero("Hero" ,100, 60,)
 bad_guy = Goblin("goblin" ,100, 20,)
@@ -43,15 +67,15 @@ bad_guy = Goblin("goblin" ,100, 20,)
 class Battle(object):
 
     def do_battle(self, hero, enemy):
-        print("=====================")
+        print("=" * 30)
         print(f"You have encountered a {enemy.name}!")
-        print("=====================")
+        print("=" * 30)
 
-        while hero.alive() and enemy.alive():
+        while hero.is_alive() and enemy.is_alive():
             hero.print_status()
             enemy.print_status()
 
-            print("-----------------------")
+            print("-" * 30)
             print("What do you want to do?")
             print(f"1. fight {enemy.name}")
             print("2. do nothing")
@@ -64,7 +88,7 @@ class Battle(object):
             elif choice == 2:
                 pass
             elif choice == 3:
-                print("You have ran away in terror")
+                print("You run away in terror")
                 exit(0)
             else:
                 print("Try again.")
@@ -72,13 +96,13 @@ class Battle(object):
 
             enemy.attack(hero)
 
-        if hero.alive():
-            print("You defeated the {}".format(enemy.name))
+        if hero.is_alive():
+            print(f"You defeated the {enemy.name}")
             return True
 
         else:
-            print("YOU LOSE!")
+            print("You are dead.")
             return False
 
-battle_start = Battle
+battle_start = Battle()
 battle_start.do_battle(cool_guy,bad_guy)
